d63ec7328c45fe6701c768b474e699c2032c1b0e
GeovannyTS <yowtecum@gmail.com>
2023-08-31 18:17:09 -0600

    Base de formularios

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  // Formulario reactivo
+
+  // public myForm: FormGroup = new FormGroup({
+  //   // name: new FormControl( '', [], [] ),
+  //   name: new FormControl('' ),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
+
+  //Formulario Builder. Es lo mismo que el group
+   public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+   })
+
+   constructor(private fb:FormBuilder){}
+
+   onSave():void{
+    console.log(this.myForm.value);
+
+   }
 
 }
