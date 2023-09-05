d13206fc7561a45daf283d3d643edaec6dccbd16
GeovannyTS <yowtecum@gmail.com>
2023-09-04 18:01:28 -0600

    Formularios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,5 +7,82 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicPageComponent {
+
+  public myFormD:FormGroup = this.fb.group({
+    name:['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Metal Slug', Validators.required]
+    ])
+  });
+
+  public newFavorite:FormControl = new FormControl('', Validators.required)
+
+  constructor(private fb:FormBuilder){}
+
+  get favoriteGames(){
+    return this.myFormD.get('favoriteGames') as FormArray
+  }
+
+  isValidField(field: string): boolean | null {
+    // myForm.controls['name'].getError('required') && myForm.controls['name'].touched
+    return this.myFormD.controls[field].errors && this.myFormD.controls[field].touched
+
+  }
+
+  isValidFieldInArray(formArray:FormArray, index:number){
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myFormD.controls[field]) return null;
+
+    const errors = this.myFormD.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es necesario';
+        case 'minlength':
+          return `Mino ${ errors ['minlength'].requiredLength } de caracteres`;
+      }
+    }
+    return '';
+  }
+
+  onDeleteFavorite(index:number):void{
+    this.favoriteGames.removeAt(index);
+  }
+
+  onAddFavorites():void{
+    if(this.newFavorite.invalid) return;
+
+    const newFavoriteGame = this.newFavorite.value;
+
+    // sin form B
+    // this.favoriteGames.push(new FormControl(newFavoriteGame, Validators.required))
+
+    // con el fomr builder
+    this.favoriteGames.push(
+      this.fb.control(newFavoriteGame, Validators.required)
+    );
+    this.newFavorite.reset();
+
+  }
+
+
+  onSunmit():void{
+
+    if(this.myFormD.invalid){
+      this.myFormD.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myFormD.value);
+    (this.myFormD.controls['favoriteGames'] as FormArray ) = this.fb.array([]);
+    this.myFormD.reset();
+  }
+
 
 }
