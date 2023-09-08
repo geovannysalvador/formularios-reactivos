46b10bd75a86b6c605848b317207ddeb537f5e89
GeovannyTS <yowtecum@gmail.com>
2023-09-08 16:13:40 -0600

    Validaciones personalizadas codigo base

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', [Validators.required]],
+    wantNotifications: [ true, [Validators.required]],
+    termsAndConditions: [false, [Validators.requiredTrue]]
+  })
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  }
+
+  constructor( private fb:FormBuilder ){}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person)
+  }
+
+  isValidField(field: string): boolean | null {
+    // myForm.controls['name'].getError('required') && myForm.controls['name'].touched
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+
+  }
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    // Para eliminar un termino si se estan mandando mas informacion de la necesaria
+    const {termsAndConditions, ...newPerson} = this.myForm.value;
+
+
+    console.log(this.myForm.value);
+
+    this.person = newPerson;
+    console.log(newPerson);
+
+
+  }
 
 }
